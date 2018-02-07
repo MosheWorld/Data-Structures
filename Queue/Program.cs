660688ddf06aeb266505530b54949937d4bfa4e2
Moshe <mmoshikoo@gmail.com>
2018-02-07 23:43:32 +0200

    Finished Queue implementation.

--- Queue/Program.cs
+++ Queue/Program.cs
@@ -6,7 +6,21 @@ namespace MosheBinieli.Queue
     {
         static void Main(string[] args)
         {
+            Queue<int> myQueue = new Queue<int>();
 
+            AddValuesToQueue(myQueue);
+
+            myQueue.Dequeue();
+            int valueFromQueue = myQueue.Dequeue();
+
+            Console.WriteLine(valueFromQueue);
+        }
+
+        private static void AddValuesToQueue(Queue<int> myQueue)
+        {
+            myQueue.Enqueue(2);
+            myQueue.Enqueue(4);
+            myQueue.Enqueue(12);
         }
     }
 }
--- Queue/Queue.cs
+++ Queue/Queue.cs
@@ -1,4 +1,68 @@
 public class Queue<T>
 {
+    #region Members
+    private Node<T> Head;
+    private Node<T> Tail;
+    private int Counter;
+    #endregion
 
+    #region Constructor
+    public Queue()
+    {
+        this.Head = null;
+        this.Tail = null;
+        this.Counter = 0;
+    }
+    #endregion
+
+    #region Public Methods
+    public void Enqueue(T newValue)
+    {
+        if (this.Head == null)
+        {
+            this.Head = new Node<T>();
+            this.Tail = this.Head;
+            this.Head.Value = newValue;
+        }
+        else
+        {
+            Node<T> tempNode = new Node<T>();
+            tempNode.Value = newValue;
+
+            tempNode.Next = this.Head;
+            this.Head.Previous = tempNode;
+
+            this.Head = tempNode;
+        }
+
+        this.Counter++;
+    }
+
+    public T Dequeue()
+    {
+        if (this.Tail == null)
+            return default(T);
+
+        T valueToReturn = this.Tail.Value;
+
+        if (this.Tail.Previous == null)
+        {
+            this.Head = null;
+            this.Tail = null;
+        }
+        else
+        {
+            this.Tail = this.Tail.Previous;
+            this.Tail.Next = null;
+        }
+
+        this.Counter--;
+        return valueToReturn;
+    }
+
+    public int Count()
+    {
+        return this.Counter;
+    }
+    #endregion
 }
