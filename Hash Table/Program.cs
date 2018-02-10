e96471bdcc6c9bd731c6e36e02a6841a7da65582
Moshe <mmoshikoo@gmail.com>
2018-02-10 16:24:51 +0200

    Added HashTable start point

---
+++ Hash Table/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MosheBinieli.Hash_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
